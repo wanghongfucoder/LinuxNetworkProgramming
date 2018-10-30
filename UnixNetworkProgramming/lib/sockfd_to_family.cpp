8d11fdf86f0157afb46d098f719513ef2f4e9f4b
LiunxPaisley <liunxalkaid@outlook.com>
2018-10-30 20:31:29 +0800

    2018-10-30-星期二20:31:29

---
+++ UnixNetworkProgramming/lib/sockfd_to_family.cpp
@@ -0,0 +1,13 @@
+#include "unp.h"
+
+#include <sys/socket.h>
+int sockfd_to_family(int sockfd){
+    struct sockaddr_storage ss;
+    socklen_t len;
+
+    len = sizeof(ss);
+    if(getsockname(sockfd, (struct sockaddr*)&ss, &len) < 0){
+        return -1;
+    }
+    return ss.ss_family;
+}
